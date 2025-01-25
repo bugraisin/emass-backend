817c01457fcdf64afae13409f37c9bf9783af723
bugraisin <bugraisin@gmail.com>
2025-01-25 17:46:25 +0300

    initial commit

---
+++ src/main/java/com/emass/emass_backend/EmassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emass.emass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmassBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emass/emass_backend/EmassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emass.emass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
