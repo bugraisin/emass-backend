1bb1f2d906ba7b1304faf7fb9f0bf1c4d5690281
bugraisin <bugraisin@gmail.com>
2025-09-05 01:13:01 +0300

    Office search request completed.

--- src/main/java/com/emass/emass_backend/model/dto/listing/details/search/OfficeSearchRequest.java
+++ src/main/java/com/emass/emass_backend/model/dto/listing/details/search/OfficeSearchRequest.java
@@ -1,43 +1,65 @@
 package com.emass.emass_backend.model.dto.listing.details.search;
 
 import com.emass.emass_backend.model.entity.enums.ListingStatus;
+import com.emass.emass_backend.model.entity.enums.ListingType;
 import com.emass.emass_backend.model.entity.enums.OfficeSubtype;
+import com.emass.emass_backend.model.entity.enums.details.FacadeDirection;
+import com.emass.emass_backend.model.entity.enums.details.HeatingType;
+import jakarta.persistence.Column;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public record OfficeSearchRequest (
+
+        ListingType listingType,
         String city,
-        String district,
-        String neighborhood,
+        List<String> district,
+        List<String> neighborhood,
         BigDecimal minPrice,
         BigDecimal maxPrice,
         ListingStatus status,
 
-        // OfficeDetails filtreleri
         OfficeSubtype subtype,
+
         Integer minNetArea,
         Integer maxNetArea,
-        Integer floorNo,
-        Integer minBuildingAge,
-        Integer maxBuildingAge,
-        Integer minRoomCount,
-        Integer maxRoomCount,
-        Integer minMeetingRoomCount,
-        Integer maxMeetingRoomCount,
+
+        List<String> floors,
+        List<String> buildingAges,
+
+        List<String> roomCount,
+        List<String> meetingRooms,
+        List<FacadeDirection> facadeDirections,
+        List<HeatingType> heatingTypes,
+
+        BigDecimal minSiteFee,
+        BigDecimal maxSiteFee,
+        BigDecimal minDeposit,
+        BigDecimal maxDeposit,
+
+        // Temel Özellikler
         Boolean furnished,
-        Boolean airConditioning,
         Boolean parking,
-        String businessCenter,
+        Boolean elevator,
+        Boolean security,
+        Boolean generator,
+
+        // Ofis Konfor
+        Boolean airConditioning,
+        Boolean internet,
+        Boolean kitchen,
+        Boolean fireSystem,
+
+        // Çalışma Alanları
         Boolean reception,
-        Integer minDeskCount,
-        Integer maxDeskCount,
-        Integer minPrivateOfficeCount,
-        Integer maxPrivateOfficeCount,
-        Integer minWorkstationCount,
-        Integer maxWorkstationCount,
-        Boolean soundIsolation,
-        BigDecimal minMaintenanceFee,
-        BigDecimal maxMaintenanceFee,
-        Boolean internetIncluded
+        Boolean waitingArea,
+        Boolean meetingRoom,
+        Boolean archive,
+        Boolean library,
+
+        Boolean serverRoom,
+        Boolean accessControl,
+        Boolean fiberInternet,
+        Boolean soundproof
 ) {}
